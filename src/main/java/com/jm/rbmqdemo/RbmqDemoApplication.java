6d6602d641234fd41f20beb0b0bbce1b4044fb51
jerry <jm90_yang@163.com>
2019-09-16 16:28:49 +0800

    原生rabbit

---
+++ src/main/java/com/jm/rbmqdemo/RbmqDemoApplication.java
@@ -0,0 +1,17 @@
+package com.jm.rbmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author User
+ */
+@SpringBootApplication
+public class RbmqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbmqDemoApplication.class, args);
+    }
+
+}
